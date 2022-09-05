4aa2a9ae0297bae1d7fe6ab9139223e90568ae93
Chris Frank <47026760+chris-kafka@users.noreply.github.com>
2022-09-05 11:18:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='cdhetero',
     version='0.1',
-    packages=find_packages(include=['cdhetero*']),
+    packages=find_packages(include=['Hetero*']),
     install_requires=['py3Dmol','absl-py','biopython',
                       'chex','dm-haiku','dm-tree',
                       'immutabledict','jax','ml-collections',
