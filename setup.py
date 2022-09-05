fab7b5ff3832edd17e40e247ca26bb9f7ebc7446
Chris Frank <47026760+chris-kafka@users.noreply.github.com>
2022-09-05 11:22:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='Hetero',
     version='0.1',
-    packages=find_packages(include=['Hetero*']),
+    packages=['Hetero'],
     install_requires=['py3Dmol','absl-py','biopython',
                       'chex','dm-haiku','dm-tree',
                       'immutabledict','jax','ml-collections',
